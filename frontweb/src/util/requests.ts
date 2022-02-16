824bc5b6face100bfee4fe20ecf9d841b23634de
Augusto Cesar <augustocesar.udi@gmail.com>
2022-02-16 20:01:17 -0300

    Saving auth data to localStorage

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,32 +1,53 @@
-import axios from "axios";
-import qs from "qs";
+import axios from 'axios';
+import qs from 'qs';
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8090';
+type LoginResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
+
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8090';
+
+const tokenKey = 'authData';
+
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
 type LoginData = {
   username: string;
   password: string;
-}
+};
 
-export const requestBackendLogin = (loginData : LoginData) => {
-  
+export const requestBackendLogin = (loginData: LoginData) => {
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    Authorization: 'Basic '+ window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-  }
+    Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+  };
 
-  const data = qs.stringify( {
+  const data = qs.stringify({
     ...loginData,
-    grant_type : 'password'
+    grant_type: 'password',
   });
 
   return axios({
-    method: 'POST', 
-    baseURL: BASE_URL, 
+    method: 'POST',
+    baseURL: BASE_URL,
     url: '/oauth/token',
     data,
-    headers
+    headers,
   });
-} 
+};
+
+export const saveAuthData = (obj: LoginResponse) => {
+  localStorage.setItem(tokenKey, JSON.stringify(obj));
+};
+
+export const getAuthData = () => {
+  const str = localStorage.getItem(tokenKey) ?? "{}";
+  return JSON.parse(str) as LoginResponse;
+};
