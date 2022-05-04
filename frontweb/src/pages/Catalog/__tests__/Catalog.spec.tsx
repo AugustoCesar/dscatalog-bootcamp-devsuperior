1fc764d4e560cae6134a2da2956cf56a0aee7b6d
Augusto Cesar <augustocesar.udi@gmail.com>
2022-05-04 19:42:45 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,6 +2,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import history from 'util/history';
 import Catalog from '..';
+import { server } from './fixtures';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('should render Catalog with products', async () => {
   render(
@@ -11,8 +16,8 @@ test('should render Catalog with products', async () => {
   );
 
   expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
-  
+
   await waitFor(() => {
     expect(screen.getByText('Smart TV')).toBeInTheDocument();
-  })
+  });
 });
